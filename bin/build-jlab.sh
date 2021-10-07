ce8ced6014eb9d4a42fb224aa767ae92c8987415
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2021-10-07 17:08:58 +0200

    log to file and tail it

--- bin/build-jlab.sh
+++ bin/build-jlab.sh
@@ -41,7 +41,8 @@ conda install -c mrtrix3 mrtrix3
 pushd /apps/tvb-hip
 wget https://fsl.fmrib.ox.ac.uk/fsldownloads/fslinstaller.py
 # 2to3 -w fslinstaller.py
-echo "" | python2 fslinstaller.py > fslinstall.log
+echo "" | python2 fslinstaller.py &> fslinstall.log
+tail fslinstall.log
 mv /usr/local/fsl ./
 
 # finally freesurfer
