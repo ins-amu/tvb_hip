a9e75b0aba9d09fc90eefed86fb83b07467034ed
Marmaduke Woodman <mmwoodman@gmail.com>
2021-11-23 15:09:57 +0100

    include wget as well

--- bin/install-packages.sh
+++ bin/install-packages.sh
@@ -8,4 +8,4 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y libnss3-dev libx11-xcb1 libxcb
 libxcursor1 libxdamage1 libxfixes3 libxi6 libxtst6 libatk1.0-0 libatk-bridge2.0-0 \
 libgdk-pixbuf2.0-0 libgtk-3-0 libgtk-3-0 libpangocairo-1.0-0 libpango-1.0-0 libcairo2 \
 libdrm2 libgbm1 libasound2 libatspi2.0-0 curl git build-essential tcsh perl nodejs \
-python2
+python2 wget
