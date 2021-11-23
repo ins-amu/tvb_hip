567bc2d3d0b4181bfd677c854d249b562db5e947
Marmaduke Woodman <mmwoodman@gmail.com>
2021-11-23 16:16:12 +0100

    reduce verbosity of wget fs and debug file sizes

--- bin/install-freesurfer.sh
+++ bin/install-freesurfer.sh
@@ -2,6 +2,6 @@
 
 mkdir -p /apps/tvb-hip
 pushd /apps/tvb-hip
-wget https://surfer.nmr.mgh.harvard.edu/pub/dist/freesurfer/7.2.0/freesurfer-linux-ubuntu18_amd64-7.2.0.tar.gz
+wget -q https://surfer.nmr.mgh.harvard.edu/pub/dist/freesurfer/7.2.0/freesurfer-linux-ubuntu18_amd64-7.2.0.tar.gz
 tar xzf freesurfer-linux-ubuntu18_amd64-7.2.0.tar.gz
 rm freesurfer-linux-ubuntu18_amd64-7.2.0.tar.gz
