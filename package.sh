04e9b6934fe82d3219be8303a5417fda5c41bbdc
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2022-11-18 19:49:10 +0100

    a few packing updates

---
+++ package.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#python -m zipapp -p '/usr/bin/env python' -c tvb_hip
+#chmod +x tvb_hip.pyz
+
+build=build
+buildtest=buildtest
+
+rm -rf $build $buildtest
+mkdir -p $build/tvb_hip
+mkdir $buildtest
+
+# create app in new folder
+pushd $build
+pip install --target tvb_hip nibabel  # dependencies that may not be available
+cp -r ../tvb_hip tvb_hip/
+echo '
+import tvb_hip.cli as cli
+cli.main()
+' > tvb_hip/__main__.py
+python -m zipapp -p '/usr/bin/env python' -c tvb_hip
+chmod +x tvb_hip.pyz
+popd
+
+# test in separate directory
+cp $build/tvb_hip.pyz $buildtest/tvb_hip.pyz
+pushd $buildtest
+# test we can invoke the cli directly
+./tvb_hip.pyz -v
+# test we can import libraries with a single file on the PYTHONPATH
+PYTHONPATH=tvb_hip.pyz python -c 'import tvb_hip; print(tvb_hip)'
+# check size of archive
+ls -lh
+popd
